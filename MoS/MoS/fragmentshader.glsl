bdf4aa9ad9528005dea5ad181c03df9a79462946
jonathanbosson <jonathan.bosson@gmail.com>
2015-02-03 09:18:39 +0100

    phong shading

--- MoS/MoS/fragmentshader.glsl
+++ MoS/MoS/fragmentshader.glsl
@@ -1,16 +1,17 @@
 #version 330 core
 
-uniform float time;
-uniform sampler2D tex;
-
-in vec3 interpolatedNormal;
-in vec2 st;
-
-out vec4 color;
-
-void main() {
-     vec4 texcolor = texture(tex, st);
-     vec3 nNormal = normalize(interpolatedNormal);
-     float diffuse = max(0.0, nNormal.z);
-     color = texcolor * diffuse;
+attribute vec4 v_coord;
+attribute vec3 v_normal;
+varying vec4 position;  // position of the vertex (and fragment) in world space
+varying vec3 varyingNormalDirection;  // surface normal vector in world space
+uniform mat4 m, v, p;
+uniform mat3 m_3x3_inv_transp;
+ 
+void main()
+{
+  position = m * v_coord;
+  varyingNormalDirection = normalize(m_3x3_inv_transp * v_normal);
+ 
+  mat4 mvp = p*v*m;
+  gl_Position = mvp * v_coord;
 }
