2021c3d431d27dc971b0feefdfe79be0ea52d149
jonathanbosson <jonathan.bosson@gmail.com>
2015-02-03 09:49:24 +0100

    fragmentshader

--- MoS/MoS/fragmentshader.glsl
+++ MoS/MoS/fragmentshader.glsl
@@ -6,13 +6,9 @@ varying vec4 position;  // position of the vertex (and fragment) in world space
 varying vec3 varyingNormalDirection;  // surface normal vector in world space
 uniform mat4 m, v, p;
 uniform mat3 m_3x3_inv_transp;
-<<<<<<< HEAD
 
 out vec4 color;
 
-=======
- 
->>>>>>> experimental
 void main()
 {
   position = m * v_coord;
