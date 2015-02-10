279d411025be3b68ec3d2289d91c556695e35ae1
jonathanbosson <jonathan.bosson@gmail.com>
2015-02-10 14:52:41 +0100

    solve merge on physicshandler

--- MoS/MoS/physicsHandler.cpp
+++ MoS/MoS/physicsHandler.cpp
@@ -35,16 +35,6 @@ void physicsHandler::calculatePosition(vector<Entity*> *theEntityList, GLFWwindo
 		glm::vec3 currPos = theEntityList->at(i)->getPosition();
 		glm::vec3 currAcc = theEntityList->at(i)->getAcceleration();
 		glm::vec3 currVel = theEntityList->at(i)->getVelocity();
-<<<<<<< HEAD
-		glm::vec3 gravity = glm::vec3(0.0f, -9.81f, 0.0f)*2.0f;
-
-		//INPUT CHECK
-
-		if (glfwGetKey(window, GLFW_KEY_LEFT)) {
-
-			currAcc = (gravity + glm::vec3(-40.0f, 0.0f, 0.0f));
-			cout << currVel.x << " " << currVel.y << " " << currVel.z << endl;
-=======
 		glm::vec3 gravity = glm::vec3(0.0f, -9.81f, 0.0f)*0.1f;
 
 		//INPUT CHECK
@@ -52,42 +42,12 @@ void physicsHandler::calculatePosition(vector<Entity*> *theEntityList, GLFWwindo
 		if (glfwGetKey(window, GLFW_KEY_KP_4)) {
 
 			currAcc = (gravity + glm::vec3(-40.0f, 0.0f, 0.0f));
->>>>>>> experimental
 		}
 		else
 		{
 			currAcc = gravity;
 		}
 
-<<<<<<< HEAD
-		if (glfwGetKey(window, GLFW_KEY_W)) {
-
-			currAcc = (gravity + glm::vec3(0.0f, 100.0f, 0.0f));
-			cout << currVel.x << " " << currVel.y << " " << currVel.z << endl;
-
-		}
-
-		if (glfwGetKey(window, GLFW_KEY_RIGHT)) {
-
-			currAcc = (gravity + glm::vec3(40.0f, 0.0f, 0.0f));
-			cout << currVel.x << " " << currVel.y << " " << currVel.z << endl;
-		}
-
-		if (glfwGetKey(window, GLFW_KEY_S)) {
-
-			currAcc = (gravity + glm::vec3(0.0f, -100.0f, 0.0f));
-			cout << currVel.x << " " << currVel.y << " " << currVel.z << endl;
-		}
-		if (glfwGetKey(window, GLFW_KEY_UP)) {
-
-			currAcc = (gravity + glm::vec3(0.0f, 0.0f, -40.0f));
-			cout << currVel.x << " " << currVel.y << " " << currVel.z << endl;
-		}
-		if (glfwGetKey(window, GLFW_KEY_DOWN)) {
-
-			currAcc = (gravity + glm::vec3(0.0f, 0.0f, 40.0f));
-			cout << currVel.x << " " << currVel.y << " " << currVel.z << endl;
-=======
 		if (glfwGetKey(window, GLFW_KEY_KP_9)) {
 
 			currAcc = (gravity + glm::vec3(0.0f, 100.0f, 0.0f));
@@ -111,7 +71,6 @@ void physicsHandler::calculatePosition(vector<Entity*> *theEntityList, GLFWwindo
 
 			currAcc = (gravity + glm::vec3(0.0f, 0.0f, 40.0f));
 
->>>>>>> experimental
 		}
 
 
@@ -131,30 +90,17 @@ void physicsHandler::resolveCollision(vector<Entity*> * theEntityList)
 	glm::vec3 iVel;
 	glm::vec3 iPos;
 	glm::vec3 jPos;
-<<<<<<< HEAD
-	glm::vec3 posVector;
-	float vLength;
-=======
 	glm::vec3 jVel;
 
 	glm::vec3 posVector;
 	float vLength;
 	float diffVel;
->>>>>>> experimental
 
 	glm::vec3 normal;
 	glm::vec3 p1Normal;
 	glm::vec3 p2Normal;
 	glm::mat4 coSystem;
 
-<<<<<<< HEAD
-	glm::vec4 reflectedNewVel;
-	glm::vec4 nreflectedNewVel;
-
-	float rad1 = 0.5f;
-	float rad2 = 0.5f;
-	for (int i = 0; i < theEntityList->size(); i++)
-=======
 	glm::vec4 reflectedNewVel_1;
 	glm::vec4 nreflectedNewVel_1;
 
@@ -166,51 +112,30 @@ void physicsHandler::resolveCollision(vector<Entity*> * theEntityList)
 	float rad1 = 0.5f;
 	float rad2 = 0.5f;
 	for (int i = 0; i < theEntityList->size() - 1; i++)
->>>>>>> experimental
 	{
 		iPos = theEntityList->at(i)->getPosition();
 		iVel = theEntityList->at(i)->getVelocity();
 
-<<<<<<< HEAD
-		for (int j = 0; j < theEntityList->size(); j++)
-		{
-			 
-			jPos = theEntityList->at(j)->getPosition();
-=======
 		for (int j = i + 1; j < theEntityList->size(); j++)
 		{
 			 
 			jPos = theEntityList->at(j)->getPosition();
 			jVel = theEntityList->at(j)->getVelocity();
->>>>>>> experimental
 
 			posVector = iPos - jPos;
 			vLength = glm::length(posVector);
 
 			if (vLength < rad1 + rad2 && i != j)
 			{
-<<<<<<< HEAD
-=======
 				float move = (rad1 + rad2 - vLength);
 
 				move = move / 2.0f;
 
->>>>>>> experimental
 				normal = posVector;
 				p1Normal = glm::cross(normal, glm::cross(normal, glm::vec3(normal.z, -normal.x, -normal.y)));
 				p2Normal = glm::cross(normal, p1Normal);
 				coSystem = glm::mat4(glm::vec4(normal, 0.0f), glm::vec4(p1Normal, 0.0f), glm::vec4(p2Normal, 0.0f), glm::vec4(0.0f, 0.0f, 0.0f, 1.0f));
 
-<<<<<<< HEAD
-				reflectedNewVel = glm::vec4(glm::reflect(iVel, normal), 1.0f);
-				nreflectedNewVel = glm::inverse(coSystem)*reflectedNewVel;
-
-				nreflectedNewVel = glm::vec4(nreflectedNewVel.x*0.5f, nreflectedNewVel.y, nreflectedNewVel.z, 1.0f);
-
-				reflectedNewVel = coSystem*nreflectedNewVel;
-
-				theEntityList->at(i)->setVelocity(glm::vec3(reflectedNewVel));
-=======
 				
 				reflectedNewVel_1 = glm::vec4(glm::reflect(iVel, normal), 1.0f);
 				nreflectedNewVel_1 = glm::inverse(coSystem)*reflectedNewVel_1;
@@ -240,7 +165,6 @@ void physicsHandler::resolveCollision(vector<Entity*> * theEntityList)
 				nBasePos = coSystem*nBasePos;
 				theEntityList->at(j)->setPosition(glm::vec3(nBasePos));
 
->>>>>>> experimental
 				//theEntityList->at(i)->setPosition(glm::vec3(currPos.x, 0.50001f, currPos.z));
 			}
 
@@ -256,16 +180,6 @@ void physicsHandler::resolveCollision(vector<Entity*> * theEntityList)
 			p2Normal = glm::cross(normal, p1Normal);
 			coSystem = glm::mat4(glm::vec4(normal, 0.0f), glm::vec4(p1Normal, 0.0f), glm::vec4(p2Normal, 0.0f), glm::vec4(0.0f, 0.0f, 0.0f, 1.0f));
 
-<<<<<<< HEAD
-			reflectedNewVel = glm::vec4(glm::reflect(currVel, normal), 1.0f);
-			nreflectedNewVel = glm::inverse(coSystem)*reflectedNewVel;
-
-			nreflectedNewVel = glm::vec4(nreflectedNewVel.x*0.5f, nreflectedNewVel.y, nreflectedNewVel.z, 1.0f);
-
-			reflectedNewVel = coSystem*nreflectedNewVel;
-
-			theEntityList->at(i)->setVelocity(glm::vec3(reflectedNewVel));
-=======
 			reflectedNewVel_1 = glm::vec4(glm::reflect(currVel, normal), 1.0f);
 			nreflectedNewVel_1 = glm::inverse(coSystem)*reflectedNewVel_1;
 
@@ -274,7 +188,6 @@ void physicsHandler::resolveCollision(vector<Entity*> * theEntityList)
 			reflectedNewVel_1 = coSystem*nreflectedNewVel_1;
 
 			theEntityList->at(i)->setVelocity(glm::vec3(reflectedNewVel_1));
->>>>>>> experimental
 			theEntityList->at(i)->setPosition(glm::vec3(currPos.x, 0.50001f, currPos.z));
 		}
 	}
