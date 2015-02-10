b7409c19d3a56e5ad9a39f0e5755303fb4073372
jonathanbosson <jonathan.bosson@gmail.com>
2015-02-10 15:05:28 +0100

    fix merge on main

--- MoS/MoS/main.cpp
+++ MoS/MoS/main.cpp
@@ -30,11 +30,8 @@ void setupViewport(GLFWwindow *window, GLfloat *P)
 
 int main()
 {
-<<<<<<< HEAD
-=======
-	srand((unsigned)time(NULL));
 
->>>>>>> experimental
+	srand((unsigned)time(NULL));
 
 	GLfloat I[16] = { 1.0f, 0.0f, 0.0f, 0.0f
 					, 0.0f, 1.0f, 0.0f, 0.0f
@@ -44,20 +41,14 @@ int main()
 					, 0.0f, 2.42f, 0.0f, 0.0f
 					, 0.0f, 0.0f, -1.0f, -1.0f
 					, 0.0f, 0.0f, -0.2f, 0.0f };
-<<<<<<< HEAD
-	GLfloat L[3] = { 0.0f, 0.0f, -3.0f };
-=======
+
 	GLfloat L[3] = { 0.0f, 10.0f, -3.0f };
 	GLfloat C[3];
->>>>>>> experimental
 
 	GLint locationMV;
 	GLint locationP;
 	GLint locationL;
-<<<<<<< HEAD
-=======
 	GLint locationColor;
->>>>>>> experimental
 
 	// start GLEW extension handler
 	if (!glfwInit()) {
@@ -91,12 +82,9 @@ int main()
 	phongShader.createShader("vertexshader.glsl", "fragmentshader.glsl");
 	MatrixStack MVstack;
 	MVstack.init();
-<<<<<<< HEAD
-=======
 
-	Camera theCamera;
+    Camera theCamera;
 
->>>>>>> experimental
 	physicsHandler theHandler;
 
 	Box theBox;
@@ -109,11 +97,7 @@ int main()
 	the2ndSphere.createSphere(0.5, 32);
 	
 	Plane thePlane;
-<<<<<<< HEAD
-	thePlane.createPlane(5.0f, 5.0f);
-=======
 	thePlane.createPlane(15.0f, 15.0f);
->>>>>>> experimental
 
 	objectList.push_back(new Sphere(glm::vec3(0.0f, 5.0f, 0.0f), 5.0f, 0.5f));
 	objectList.push_back(new Sphere(glm::vec3(0.0f, 8.0f, 0.0f), 5.0f, 0.5f));
@@ -126,10 +110,8 @@ int main()
 	locationMV = glGetUniformLocation(phongShader.programID, "MV");
 	locationP = glGetUniformLocation(phongShader.programID, "P");
 	locationL = glGetUniformLocation(phongShader.programID, "lightPosition");
-<<<<<<< HEAD
-=======
+
 	locationColor = glGetUniformLocation(phongShader.programID, "objectColor");
->>>>>>> experimental
 
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
@@ -142,18 +124,12 @@ int main()
 	glm::vec3 pos = glm::vec3(0.0f);
 	while (!glfwWindowShouldClose(window)) {
 
-<<<<<<< HEAD
-		
-		//GL calls
-		glClearColor(0.0f, 0.1f, 0.0f, 0.0f);
-=======
 		if (glfwGetKey(window, GLFW_KEY_O)) {
 			objectList.push_back(new Sphere(glm::vec3(0.0f, 8.0f, 0.0f), 5.0f, 0.5f));
 		}
 		
 		//GL calls
 		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
->>>>>>> experimental
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glEnable(GL_DEPTH_TEST);
 		glEnable(GL_CULL_FACE);
@@ -162,25 +138,12 @@ int main()
 
 		//Send static variables to vertexshader
 		glUniformMatrix4fv(locationP, 1, GL_FALSE, P);
-<<<<<<< HEAD
-		glUniform3fv(locationL, 1, L);
-=======
-		
->>>>>>> experimental
 
 		setupViewport(window, P);
 
 		//TIME
 		theHandler.calculateTime();
-<<<<<<< HEAD
 
-		//Transform calculations and rendering
-		MVstack.push();
-		MVstack.translate(glm::vec3(0.0f, -2.0f, -10.5f));
-			MVstack.push();
-			MVstack.rotZ(-0.1);
-				glUniformMatrix4fv(locationMV, 1, GL_FALSE, MVstack.getCurrentMatrix());
-=======
 		theCamera.poll(window);
 
 		//Transform calculations and rendering
@@ -199,7 +162,6 @@ int main()
 				glUniform3fv(locationL, 1, L);
 				glUniform3fv(locationColor, 1, C);
 
->>>>>>> experimental
 				thePlane.render();
 			MVstack.pop();
 
@@ -215,15 +177,12 @@ int main()
 					MVstack.translate(oPointer->getPosition());
 					glUniformMatrix4fv(locationMV, 1, GL_FALSE, MVstack.getCurrentMatrix());
 					oPointer = objectList[i];
-<<<<<<< HEAD
-=======
 
 					C[0] = oPointer->getColorR();
 					C[1] = oPointer->getColorG();
 					C[2] = oPointer->getColorB();
 
 					glUniform3fv(locationColor, 1, C);
->>>>>>> experimental
 					oPointer->render();
 				MVstack.pop();
 			}
